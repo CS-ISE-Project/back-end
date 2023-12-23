56ef53796c99eb239b7022bf3435eef899d432c1
4zz0u4k <la_akeb@esi.dz>
2023-12-23 21:25:25 +0100

    added closing for elasticsearch sessions

--- app/services/elasticsearch_service.py
+++ app/services/elasticsearch_service.py
@@ -4,8 +4,12 @@ from elasticsearch import Elasticsearch
 from app.elasticsearch.setup import get_es_instance
 
 def get_document(document_id: int,es : Elasticsearch = get_es_instance()):
-    return es.get(index=INDEX_NAME, id=document_id)
+    response = es.get(index=INDEX_NAME, id=document_id)
+    es.transport.close()
+    return response
     
         
 def index_document(document: ArticleModel,es : Elasticsearch = get_es_instance()):
-    return es.index(index=INDEX_NAME,id=1, body=document.dict())
+    response =  es.index(index=INDEX_NAME,id=1, body=document.dict())
+    es.transport.close()
+    return response
