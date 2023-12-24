df0fbb64499038d6f0128b85ae2cc1c67a77e29e
4zz0u4k <la_akeb@esi.dz>
2023-12-24 01:09:47 +0100

    one instance of Elasticsearch to do the job

--- app/elasticsearch/setup.py
+++ app/elasticsearch/setup.py
@@ -1,10 +1,13 @@
 from app.config.creds import ELASTICSEARCH_HOST,ELASTICSEARCH_PASSWORD,ELASTICSEARCH_PORT,ELASTICSEARCH_SCHEME,ELASTICSEARCH_USERNAME
 from elasticsearch import Elasticsearch
 
-def get_es_instance():
+
+def creat_es_instance():
     return Elasticsearch(
         [
             {'host': ELASTICSEARCH_HOST, 'port': int(ELASTICSEARCH_PORT), "scheme": ELASTICSEARCH_SCHEME}
         ],
             basic_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD)
     )
+    
+es = creat_es_instance()
--- app/services/elasticsearch_service.py
+++ app/services/elasticsearch_service.py
@@ -1,15 +1,11 @@
 from app.models.article import ArticleModel
 from app.config.creds import INDEX_NAME
 from elasticsearch import Elasticsearch
-from app.elasticsearch.setup import get_es_instance
+from app.elasticsearch.setup import es
 
-def get_document(document_id: int,es : Elasticsearch = get_es_instance()):
-    response = es.get(index=INDEX_NAME, id=document_id)
-    es.transport.close()
-    return response
+def get_document(document_id: int):
+    return es.get(index=INDEX_NAME, id=document_id)
     
         
-def index_document(document: ArticleModel,es : Elasticsearch = get_es_instance()):
-    response =  es.index(index=INDEX_NAME,id=1, body=document.dict())
-    es.transport.close()
-    return response
+def index_document(document: ArticleModel):
+    return es.index(index=INDEX_NAME,id=2, body=document.dict())
