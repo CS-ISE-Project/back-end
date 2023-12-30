77a69f255df7e3ff54d0d1f4c4475b5bc9fd7643
BrouthenKamel <lk_brouthen@esi.dz>
2023-12-30 12:10:48 +0100

    Article Extraction

--- app/utils/text.py
+++ app/utils/text.py
@@ -1,6 +1,6 @@
 import re
 
 def clean_text(text):
-    cleaned_text = re.sub(r'[^\w\s.,:?!\'"-+=()[\]{}*&%$#@;/|]', '', text)
+    cleaned_text = re.sub(r'[^\w\s.,:;/?!\'"-+=()[\]{}*%$@]', '', text)
     cleaned_text = ' '.join(cleaned_text.split())
     return cleaned_text
