fe9d52e27877204d46531b96e9000e69b005e592
BrouthenKamel <lk_brouthen@esi.dz>
2024-01-10 14:52:07 +0100

    Article endpoints permissions

--- app/routers/article_router.py
+++ app/routers/article_router.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Depends
 
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from app.utils.jwt import verify_token
+
 from sqlalchemy.orm import Session
 from app.scripts.database.setup import get_db
 
@@ -7,20 +10,25 @@ from app.models.article import ArticleModel
 
 from app.controllers.article_controller import get_article_controller, create_article_controller, update_article_controller, delete_article_controller
 
+auth_scheme=HTTPBearer()
 router = APIRouter()
 
 @router.get("/{article_id}", response_model=ArticleModel)
-def read_article(article_id: int, db: Session = Depends(get_db)):
+def read_article(article_id: int, db: Session = Depends(get_db), token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    verify_token(token.credentials, ['user', 'moderator', 'admin'])
     return get_article_controller(article_id, db)
 
 @router.post("/", response_model=ArticleModel)
-def create_article(article: ArticleModel, db: Session = Depends(get_db)):
+def create_article(article: ArticleModel, db: Session = Depends(get_db), token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    verify_token(token.credentials, ['admin'])
     return create_article_controller(article, db)
 
 @router.put("/{article_id}", response_model=ArticleModel) 
-def update_article(article_id: int, updated_article: ArticleModel, db: Session = Depends(get_db)):
+def update_article(article_id: int, updated_article: ArticleModel, db: Session = Depends(get_db), token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    verify_token(token.credentials, ['moderator'])
     return update_article_controller(article_id, updated_article, db)
 
 @router.delete("/{article_id}" , response_model=ArticleModel)
-def delete_article(article_id: int, db: Session = Depends(get_db)):
+def delete_article(article_id: int, db: Session = Depends(get_db), token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    verify_token(token.credentials, ['moderator'])
     return delete_article_controller(article_id, db)
