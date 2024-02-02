1d77bd52743fc0ed4f43fcf8bfb081184f5ae2b5
4zz0u4k <la_akeb@esi.dz>
2024-02-02 01:47:21 +0100

    filtering is now conform to the es package version and mapping is added

--- app/scripts/es/setup.py
+++ app/scripts/es/setup.py
@@ -1,4 +1,4 @@
-from app.config.creds import ELASTICSEARCH_URL, ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD 
+from app.config.creds import ELASTICSEARCH_URL, ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD,INDEX_NAME
 
 from elasticsearch import Elasticsearch
     
@@ -6,3 +6,24 @@ es = Elasticsearch(
     ELASTICSEARCH_URL,
     http_auth=(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD)
 )
+try:
+    resp = es.indices.put_mapping(
+        index=INDEX_NAME,
+        body={
+            "properties": {
+                "url": {"type": "text"},
+                "title": {"type": "text"},
+                "authors": {"type": "text"},
+                "institutes": {"type": "text"},
+                "keywords": {"type": "text"},
+                "abstract": {"type": "text"},
+                "content": {"type": "text"},
+                "references": {"type": "text"},
+                "publication_date": {"type": "date"}
+            }
+        },
+    )
+    if resp.get("acknowledged"):
+        print("Index mapped successfully")
+except Exception as e:
+    print("An error occurred while mapping the index :", e)
--- app/services/es_service.py
+++ app/services/es_service.py
@@ -6,6 +6,7 @@ from app.scripts.es.setup import es
 
 from app.models.article import ArticleModel
 from app.models.query import AdvanceQueryModel
+from app.models.filter import FilterModel
 
 def get_document(document_id: int):
     document = es.get(index=INDEX_NAME, id=document_id)
@@ -115,3 +116,107 @@ def advanced_query_search(query: AdvanceQueryModel):
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail=f"An error occurred while performing advanced non restricted search. Error: {str(e)}"
             )
+
+def filter_search(search : str|AdvanceQueryModel , filter : FilterModel):
+    filter_clauses = [
+            {"terms": {"authors": filter.authors}} if filter.authors else None,
+            {"terms": {"keywords": filter.keywords}} if filter.keywords else None,
+            {"terms": {"institues": filter.institues}} if filter.institues else None,
+            {"range": {
+                "dateField": {
+                    "gte": filter.date_interval[0],
+                    "lt": filter.date_interval[1]
+                    }
+                }
+            } if filter.date_interval else None
+        ]
+    filter_clauses = [clause for clause in filter_clauses if clause is not None]
+    
+    # Filtering from a simple Search
+    if isinstance(search,str):
+        try:
+            res = es.search(
+                index=INDEX_NAME,
+                body={
+                    "query": {
+                        "bool": {
+                            "must" : {
+                                "multi_match": {
+                                    "query": search,
+                                    "fields": ["title", "abstract", "keywords", "content", "authors", "institutes", "references"]
+                                }
+                            },
+                            "filter" : filter_clauses
+                        }
+                    }
+                }
+            )
+            if res['hits']['total']['value'] == 0:
+                return []
+            return [ArticleModel(**r['_source']) for r in res['hits']['hits']]
+        
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"An error occurred while filtering simple search results. Error: {str(e)}"
+            )
+    
+    # Filtering from an Advanced Search
+    elif isinstance(search,AdvanceQueryModel):
+        if(query.restricted):
+            must_clauses = [
+                {"match": {"title": query.title}} if query.title else None,
+                {"match": {"keywords": query.keywords}} if query.keywords else None,
+                {"match": {"content": query.content}} if query.content else None,
+                {"match": {"authors": query.authors}} if query.authors else None,
+                {"match": {"institutes": query.institutes}} if query.institutes else None,
+            ]
+            must_clauses = [clause for clause in must_clauses if clause is not None]
+            try:
+                res = es.search(
+                    index=INDEX_NAME,
+                    body={
+                        "query": {
+                            "bool" : {
+                                "must" : must_clauses,
+                                "filter" : filter_clauses
+                            }
+                        }
+                    }
+                )
+                if res['hits']['total']['value'] == 0:
+                    return []
+                return [ArticleModel(**r['_source']) for r in res['hits']['hits']]
+            
+            except Exception as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"An error occurred while fltering advanced restricted search results. Error: {str(e)}"
+                )
+        else:
+            try:
+                res = es.search(
+                    index=INDEX_NAME,
+                    body={
+                        "query": {
+                            "bool" : {
+                                "should" : [
+                                    { "match" : {"title" : query.title} },
+                                    { "match" : {"keywords" : query.keywords} },
+                                    { "match" : {"content" : query.content} },
+                                    { "match" : {"authors" : query.authors} },
+                                    { "match" : {"institutes" : query.institutes} },
+                                ],
+                                "filter" : filter_clauses,
+                            }
+                        }
+                    }
+                )
+                if res['hits']['total']['value'] == 0:
+                    return []
+                return [ArticleModel(**r['_source']) for r in res['hits']['hits']]
+            except Exception as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"An error occurred while filtering advanced non restricted search results. Error: {str(e)}"
+                )
